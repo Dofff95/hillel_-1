a6dca82f4f8dbf8025a213f012404a845dd681d8
Ivan Kr <126171248+Dofff95@users.noreply.github.com>
2024-01-28 16:26:22 +0200

    1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Button from './components/Button/Button';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hillel project</h1>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
